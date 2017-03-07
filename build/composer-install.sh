e199756c8575fae2bf5e1f91d2c49940a2c07f26
Rafal Kozlowski <kozlowski.rafal@gmail.com>
2017-03-07 01:18:21 +0100

    update target composer file name

--- build/composer-install.sh
+++ build/composer-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -O composer-setup.php https://getcomposer.org/installer
-php composer-setup.php --install-dir=bin --filename=composer_test
+php composer-setup.php --install-dir=bin --filename=composer
 rm composer-setup.php
